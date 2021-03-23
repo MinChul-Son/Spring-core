d6e0913736be4be30a1e0a24a327fd1919bad166
MinChul-Son <smc5236@naver.com>
2021-03-23 20:01:27 +0900

    스프링 빈 등록 : 자동을 기본으로, 특정 경우만 수동으로
    Component / Autowired 애노테이션을 사용한 자동 빈 등록이 훨씬 간편하고
    편하지만, 스프링에서 지원하는 것이 아닌 내가 직접 등록하는 기술 지원
    객체나 다형성을 적극적으로 활용하는 비즈니스 로직은 수동 빈 등록을
    고려해보자. 이런 경우에는 설정 정보가 명확하게 들어나있는 것이
    한눈에 파악하기에 쉽다. 그래도 자동을 쓰고싶다면 패키지로 관련 클래스를
    모아두자.

---
+++ src/main/java/hello/core/order/DiscountAppConfig.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DiscountAppConfig {
+
+    @Bean
+    public DiscountPolicy rateDiscountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
+    @Bean
+    public DiscountPolicy fixDiscountPolicy() {
+        return new FixDiscountPolicy();
+    }
+}
